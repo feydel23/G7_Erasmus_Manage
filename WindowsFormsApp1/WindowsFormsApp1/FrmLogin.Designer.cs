a57e4165936e5949b03bdde4cd6cd86948973b55
feydel23 <afeydel23@student.foi.hr>
2023-05-06 17:35:23 +0200

    Creates class.

--- WindowsFormsApp1/WindowsFormsApp1/FrmLogin.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/FrmLogin.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace Erasmus_Manager
 {
     partial class FrmLogin
     {
@@ -51,7 +51,7 @@
             this.label1.Size = new System.Drawing.Size(68, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Full Name";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
